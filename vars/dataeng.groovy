f399d8d10b3523594f49d748ba6724c8527405b1
Andy D <mindovermiles262@gmail>
2020-02-18 11:00:13 -0800

    Tue Feb 18 11:00:13 PST 2020

--- vars/dataeng.groovy
+++ vars/dataeng.groovy
@@ -1,5 +1,20 @@
 #!/usr/bin/env groovy
 
+// Filename:  dataeng.groovy
+// Purpose:   Helper methods for DataEng team
+// Usage:     Inside of project repository, include Jenkinsfile to pull this
+//            library and run 'dataeng()'
+//
+// Usage Example:
+//            library identifier: 'prod-jgl@master', retriever: modernscm(
+//              [$class: 'gitscmsource', remote: 'https://github.com/mindovermiles262/jgl'])
+//            dataeng()
+// Methods:
+//   call() => Runs entire build pipeline
+//   verifyBranchName() => Checks that branch is 'master', 'feature/*' or 'develop'. Fails if not
+//   unitTest( env.GIT_URL,   ) 
+
+
 def call() {
   pipeline {
     agent {
@@ -40,7 +55,7 @@ def call() {
           }
         }
         steps {
-          unitTest(env.GIT_URL)
+          unitTest()
         }
       }
 
@@ -56,15 +71,12 @@ def verifyBranchName(String regexPattern = "(^(origin/)?master\$|^feature/.*|^de
   }
 }
 
-def overwriteMap(Map defaultSettings, Map customSettings) {
-  customSettings.each{ entry -> defaultSettings[entry.key] = entry.value }
-  return defaultSettings
-}
 
-// Runs 'make test' on specified git repository. Defaults to a python testing
-// environment. Pass a map as the second argument for custom settings
-def unitTest(String unitTestGitUrl, Map customSettings = [:]) {
+// Runs 'make test' on specified git repository. Pass a map as the second 
+// argument for custom settings
+def unitTest(Map customSettings = [:]) {
   defaultSettings = [
+    unitTestGitUrl: env.GIT_URL
     unitTestGitBranch: env.GIT_BRANCH,
     unitTestMakefile: "Makefile",
     unitTestLanguage: "python",
@@ -73,12 +85,15 @@ def unitTest(String unitTestGitUrl, Map customSettings = [:]) {
 
   settings = overwriteMap(defaultSettings, customSettings)
 
-  switch(settings['unitTestLanguage']){
+  switch(settings.unitTestLanguage){
   case("python"):
     pipeline {
-      container(settings['unitTestContainer']) {
-        checkout([$class: 'GitSCM', branches: [[name: settings['unitTestGitBranch']]],
-            userRemoteConfigs: [[url: unitTestGitUrl]]])
+      container(settings.unitTestContainer) {
+        checkout([
+          $class: 'GitSCM',
+          branches: [[name: settings.unitTestGitBranch]],
+          userRemoteConfigs: [[url: settings.unitTestGitUrl]]
+        ])
         sh "make -f ${settings['unitTestMakefile']} test"
       }
     }
@@ -86,4 +101,11 @@ def unitTest(String unitTestGitUrl, Map customSettings = [:]) {
     // Fail if not 'unitTestLanguage' is not supported
     error("[!] Unit Testing Language not supported.")
   }
+}
+
+
+// Given two maps, append or overwrite the second into the first
+def overwriteMap(Map defaultSettings, Map customSettings) {
+  customSettings.each{ entry -> defaultSettings[entry.key] = entry.value }
+  return defaultSettings
 }
