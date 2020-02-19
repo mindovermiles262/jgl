fd638b8d9c1ea7003a66b2adf779a7879aa87748
Andy D <mindovermiles262@gmail>
2020-02-19 12:03:28 -0800

    Wed Feb 19 12:03:28 PST 2020

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -25,34 +25,34 @@ def call(){
 
       // immediately fail the job when someone is working
       // with a branch we know nothing about.
-      stage('verify branch') {
-        steps {
-          script {
-            dataeng.verifyBranchName("(^(origin/)?master\$|^feature/.*|^develop\$)")
-          }
-        }
-      }
+      // stage('verify branch') {
+      //   steps {
+      //     script {
+      //       dataeng.verifyBranchName("(^(origin/)?master\$|^feature/.*|^develop\$)")
+      //     }
+      //   }
+      // }
 
       // Runs 'make test' to execute unit testing. Add an optional map config
       // to dataeng.unitTest() to change default values.
-      stage('unit test') {
-        agent {
-          kubernetes {
-            containerTemplate {
-              // TODO: Upload python testing container to GCR with 'make' installed
-              image 'mindovermiles262/pytest:0.2.0'
-                name 'dataeng-pytest'
-                command 'cat'
-                ttyEnabled true
-            }
-          }
-        }
-        steps {
-          script{
-            dataeng.unitTest()
-          }
-        }
-      }
+      // stage('unit test') {
+      //   agent {
+      //     kubernetes {
+      //       containerTemplate {
+      //         // TODO: Upload python testing container to GCR with 'make' installed
+      //         image 'mindovermiles262/pytest:0.2.0'
+      //           name 'dataeng-pytest'
+      //           command 'cat'
+      //           ttyEnabled true
+      //       }
+      //     }
+      //   }
+      //   steps {
+      //     script{
+      //       dataeng.unitTest()
+      //     }
+      //   }
+      // }
 
       stage('Build image') {
         steps {
@@ -60,6 +60,7 @@ def call(){
             def localSettings = readYaml(file: "resources/dataeng-props.yml")
             def props = dataeng.configLocal(localSettings)
             println "${props.getClass()}" 
+            println "${props.repo}"
           }
         }
       }
