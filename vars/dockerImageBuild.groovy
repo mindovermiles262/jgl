af5ba04249e08151ae79843d94d59ce0d197c0dd
Andy D <mindovermiles262@gmail>
2020-02-19 11:54:00 -0800

    Wed Feb 19 11:54:00 PST 2020

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,73 +1,66 @@
 def call(){
 
-pipeline {
+  pipeline {
 
-  agent {
-    kubernetes {
-      containerTemplate {
-        name 'gcloud'
-        image 'google/cloud-sdk:267.0.0-alpine'
-        ttyEnabled true
-        command 'cat'
-      }
-    }
-  }
-
-  stages {
-    // List ENV vars for easier debugging
-    stage('Get ENV') {
-      steps {
-        script {
-          sh 'printenv'
-        }
-      }
-    }
-
-    // immediately fail the job when someone is working
-    // with a branch we know nothing about.
-/*
-    stage('verify branch') {
-      steps {
-        script {
-          dataeng.verifyBranchName("(^(origin/)?master\$|^feature/.*|^develop\$)")
-        }
-      }
-    }
-*/
-
-    // Runs 'make test' to execute unit testing. Add an optional map config
-    // to dataeng.unitTest() to change default values.
-/*
-    stage('unit test') {
-      agent {
-        kubernetes {
-          containerTemplate {
-            // TODO: Upload python testing container to GCR with 'make' installed
-            image 'mindovermiles262/pytest:0.2.0'
-            name 'dataeng-pytest'
-            command 'cat'
+    agent {
+      kubernetes {
+        containerTemplate {
+          name 'gcloud'
+            image 'google/cloud-sdk:267.0.0-alpine'
             ttyEnabled true
+            command 'cat'
+        }
+      }
+    }
+
+    stages {
+      // List ENV vars for easier debugging
+      stage('Get ENV') {
+        steps {
+          script {
+            sh 'printenv'
           }
         }
       }
-      steps {
-        script{
-          dataeng.unitTest()
+
+      // immediately fail the job when someone is working
+      // with a branch we know nothing about.
+      stage('verify branch') {
+        steps {
+          script {
+            dataeng.verifyBranchName("(^(origin/)?master\$|^feature/.*|^develop\$)")
+          }
         }
       }
-    }
-*/
-  stage('Build image') {
-    steps {
-      script {
-        def localSettings = readYaml(file: "resources/dataeng-props.yml")
-        def props = dataeng.configLocal(localSettings)
-        props.each {
-          println "${it.key} => ${it.value}"
+
+      // Runs 'make test' to execute unit testing. Add an optional map config
+      // to dataeng.unitTest() to change default values.
+      stage('unit test') {
+        agent {
+          kubernetes {
+            containerTemplate {
+              // TODO: Upload python testing container to GCR with 'make' installed
+              image 'mindovermiles262/pytest:0.2.0'
+                name 'dataeng-pytest'
+                command 'cat'
+                ttyEnabled true
+            }
+          }
+        }
+        steps {
+          script{
+            dataeng.unitTest()
+          }
+        }
+      }
+      stage('Build image') {
+        steps {
+          script {
+            def localSettings = readYaml(file: "resources/dataeng-props.yml")
+            def props = dataeng.configLocal(localSettings)
+          }
         }
       }
     }
   }
-}
-}
 }
