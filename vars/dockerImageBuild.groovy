8dbdd64765261504d034d02b8a3d0e0c9d45b941
Andy D <mindovermiles262@gmail>
2020-02-19 12:51:10 -0800

    Wed Feb 19 12:51:10 PST 2020

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -15,44 +15,63 @@ pipeline {
 
   stages {
     // List ENV vars for easier debugging
-    stage('Get ENV') {
-      steps {
-        script {
-          sh 'printenv'
-        }
-      }
-    }
+    // stage('Get ENV') {
+    //   steps {
+    //     script {
+    //       sh 'printenv'
+    //     }
+    //   }
+    // }
 
     // immediately fail the job when someone is working
     // with a branch we know nothing about.
-    stage('verify branch') {
-      steps {
-        script {
-          dataeng.verifyBranchName("(^(origin/)?master\$|^feature/.*|^develop\$)")
-        }
-      }
-    }
+    // stage('verify branch') {
+    //   steps {
+    //     script {
+    //       dataeng.verifyBranchName("(^(origin/)?master\$|^feature/.*|^develop\$)")
+    //     }
+    //   }
+    // }
 
     // Runs 'make test' to execute unit testing. Add an optional map config
     // to dataeng.unitTest() to change default values.
-    stage('unit test') {
+    // stage('unit test') {
+    //   agent {
+    //     kubernetes {
+    //       containerTemplate {
+    //         // TODO: Upload python testing container to GCR with 'make' installed
+    //         image 'mindovermiles262/pytest:0.2.0'
+    //         name 'dataeng-pytest'
+    //         command 'cat'
+    //         ttyEnabled true
+    //       }
+    //     }
+    //   }
+    //   steps {
+    //     script{
+    //       dataeng.unitTest()
+    //     }
+    //   }
+    // }
+    
+    stage('Docker Build') {
       agent {
         kubernetes {
           containerTemplate {
-            // TODO: Upload python testing container to GCR with 'make' installed
-            image 'mindovermiles262/pytest:0.2.0'
-            name 'dataeng-pytest'
+            image 'docker'
+            name 'docker-build'
             command 'cat'
             ttyEnabled true
           }
         }
       }
-      steps {
-        script{
-          dataeng.unitTest()
+      steps('Build Image') {
+        script {
+          sh "echo Hello World"
         }
       }
     }
+
   }
 }
 }
